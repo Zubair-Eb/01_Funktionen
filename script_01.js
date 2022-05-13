77e08413701cb87ef288ffe30bbe2d0df6cd821f
Zubair Yousaf-Eberl <103026463+Zubair-Eb@users.noreply.github.com>
2022-05-13 14:03:27 +0200

    Parameter

--- script_01.js
+++ script_01.js
@@ -3,4 +3,29 @@
 /***** Funktionen 01 *****/
 
 // 1. Kapselung von Codeblöcken
+// test();
 
+// ausgabeNamen2("Rocky");
+// ausgabeNamen2("David");
+// ausgabeNamen2("Atanas");
+ausgabeNamenParams("Maxine", "Mützerich");
+ausgabeNamenParams(prompt("Vorname?"),prompt("Nachname?"));
+
+
+function test()
+{
+console.log("Grizabella");
+}
+
+/***** Funktion 02a ******/
+
+function ausgabeNamen2(firstName) {
+    
+    console.log("Hallo "+ firstName + "!");
+    
+}
+
+
+function ausgabeNamenParams(firstName,familyName) {
+    console.log("Hallo " + firstName + " " + familyName + "!");
+    }
